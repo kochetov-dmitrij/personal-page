ec27a0b37097f2f1ff163602733eb957bc7f7d4e
d.kochetov <d.kochetov98@gmail.com>
2023-07-20 14:08:40 +0400

    Fix volumes

--- terraform/server.tf
+++ terraform/server.tf
@@ -20,20 +20,22 @@ resource "scaleway_instance_security_group" "personal_page" {
 }
 
 resource "scaleway_instance_server" "personal_page" {
-  type       = "DEV1-S"
-  image      = "ubuntu_jammy"
+  name  = "personal_page"
+  type  = "DEV1-S"
+  image = "ubuntu_jammy"
 
   ip_id = scaleway_instance_ip.personal_page_public_ip.id
-  additional_volume_ids = [scaleway_instance_volume.personal_page_data.id]
+  #  additional_volume_ids = [scaleway_instance_volume.personal_page_data.id]
 
   root_volume {
-    size_in_gb = 15
+    volume_type = "l_ssd"
+    size_in_gb  = 15
   }
 
   security_group_id = scaleway_instance_security_group.personal_page.id
 }
 
-resource "scaleway_instance_volume" "personal_page_data" {
-  size_in_gb = 15
-  type       = "b_ssd"
-}
+#resource "scaleway_instance_volume" "personal_page_data" {
+#  size_in_gb = 15
+#  type       = "b_ssd"
+#}
