541031996e1384b038a5c1cf407234d9ac688f97
d.kochetov <d.kochetov98@gmail.com>
2023-07-20 18:44:34 +0400

    Add records

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -2,13 +2,13 @@ data "cloudflare_accounts" "all" {
   name = "example account"
 }
 
-resource "cloudflare_zone" "kek-kochetov-dev" {
-  zone = "kek.kochetov.dev"
+resource "cloudflare_zone" "kekochetov-dev" {
+  zone = "kekochetov.dev"
   account_id = var.cloudflare_account_id
 }
 
 resource "cloudflare_record" "cname-lol" {
-  zone_id = cloudflare_zone.kek-kochetov-dev.id
+  zone_id = cloudflare_zone.kekochetov-dev.id
   name    = "lol"
   value   = "example.com"
   type    = "CNAME"
