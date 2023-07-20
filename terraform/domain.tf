476a0dfd775626640238f13f02eb1781d9c2cf70
d.kochetov <d.kochetov98@gmail.com>
2023-07-20 18:59:00 +0400

    Add records

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -2,25 +2,24 @@ data "cloudflare_accounts" "all" {
   name = "example account"
 }
 
-resource "cloudflare_zone" "kekochetov-dev" {
-  zone = "kekochetov.dev"
-  account_id = var.cloudflare_account_id
+data "cloudflare_zone" "kochetov-dev" {
+  zone = "kochetov.dev"
 }
 
 resource "cloudflare_record" "cname-apex" {
-  zone_id = cloudflare_zone.kekochetov-dev.id
+  zone_id = data.cloudflare_zone.kochetov-dev.id
   name    = "@"
   value   = "www.kochetov.dev"
   type    = "CNAME"
-  ttl     = 3600
+  ttl     = 600
   proxied = false
 }
 
 resource "cloudflare_record" "cname-www" {
-  zone_id = cloudflare_zone.kekochetov-dev.id
+  zone_id = data.cloudflare_zone.kochetov-dev.id
   name    = "www"
   value   = "kochetov-dmitrij.github.io"
   type    = "CNAME"
-  ttl     = 3600
+  ttl     = 600
   proxied = false
 }
